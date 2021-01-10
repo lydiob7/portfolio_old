936dead0894f31989166b9d1da8acb5492dc3166
Tomi Scattiniclear <tomasscattini@gmail.com>
2021-01-10 18:31:45 -0500

    build in backend

--- backend/app.js
+++ backend/app.js
@@ -18,7 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/build')));
 
 // default value for title local
 app.locals.title = 'Tomas Scattini - Personal Portfolio';
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,7 +4,7 @@ const { contactEmail } = require('../config/nodemailer')
 
 /* GET home page */
 router.get('/', (req, res, next) => {
-  res.status(200).json({'message': 'working'});
+  res.status(200).send('index.html');
 });
 
 /* Contact form */
